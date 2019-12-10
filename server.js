9225bf9ca7c7da798959607dff429084f129a530
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-12-10 18:20:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ mongoose.connect('mongodb://localhost:27017/todoapp', {
     console.log("Connecting to database failed...")
 })
 
-let er = '';
-let logEr = '';
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -28,16 +25,50 @@ app.engine('mustache', mustacheExpressInstance);
 app.set('view ewngine', 'mustache');
 app.set('views', __dirname + '/views');
 
+// ZMIENNE DO WYŚWIETLANIA BŁĘDÓW
+let er = '';
+let logEr = '';
+let regEr = '';
 
+
+// LOGIN 
 app.get('/', (req, res) => {
     res.render('login.mustache', { logEr: logEr })
 })
 
+// REGISTER
+app.get('/register', (req, res) => {
+    logEr = '';
+    res.render('register.mustache', { regEr: regEr })
+})
+
+app.post('/register', (req, res) => {
+
+    const { error } = validateUser(req.body.login, req.body.password);
+    if (error) {
+        regEr = `Error: ${error.details[0].message}`
+        res.redirect("/register");
+    } else {
+        let newUser = new User({ login: req.body.login, password: req.body.password });
+        newUser.save()
+            .then(result => {
+                logEr = '';
+                regEr = '';
+                res.redirect('/')
+            });
+    }
+})
+
+
+// LISTA TODO
 app.get('/todo', (req, res) => {
     logEr = '';
     Task.find({}).then((results) => res.render('index.mustache', { tasks: results, er: er }))
 })
 
+
+
+//SPRAWDZENIE CZY JEST KONTO I OTRZYMANIE LISTY
 app.post('/todo', async(req, res) => {
     let login = req.body.login;
     let password = req.body.password;
@@ -52,12 +83,13 @@ app.post('/todo', async(req, res) => {
     }
 });
 
+//NOWY TASK
 app.post('/newtask', (req, res) => {
-
     const { error } = validateTask(req.body.name);
     if (error) {
         er = `Error: ${error.details[0].message}`
-        res.redirect("/todo");
+        logEr = '';
+        res.redirect('/todo')
     } else {
         let newTask = new Task({ name: req.body.name });
         newTask.save()
@@ -68,14 +100,28 @@ app.post('/newtask', (req, res) => {
     }
 })
 
+//USUNIĘCIE TASKA
 app.get('/task/:id/delete', (req, res) => {
-    Task.findByIdAndRemove(req.params.id).then(() => res.redirect('/todo'))
+    Task.findByIdAndRemove(req.params.id).then(() => {
+        er = '';
+        res.redirect('/todo')
+    })
 })
 
+
+//WALIDACJA TASKA I UŻYTKOWANIKA
 function validateTask(task) {
     const schema = Joi.string().min(3).required();
 
     return Joi.validate(task, schema);
+}
+
+function validateUser(login, password) {
+
+    loginSchema = Joi.string().min(5).required();
+    passwordSchema = Joi.string().min(5).required();
+
+    return Joi.validate(login, loginSchema) && Joi.validate(password, passwordSchema);
 }
 
 
