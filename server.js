17f17b3d59e8a3de61acc5b60c0a3577e157094a
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-12-14 10:45:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -30,15 +30,9 @@ let logEr = '';
 let regEr = '';
 
 
-
-// // INVALID PAGE
-// app.get('/*', (req, res) => {
-//     res.send("INVALID PAGE")
-// })
-
-
 // LOGIN 
 app.get('/', (req, res) => {
+    regEr = '';
     res.render('login.mustache', { logEr: logEr })
 })
 
@@ -48,11 +42,15 @@ app.get('/register', (req, res) => {
     res.render('register.mustache', { regEr: regEr })
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async(req, res) => {
 
     const { error } = validateUser(req.body.login, req.body.password);
+    var user = await User.findOne({ login: req.body.login, password: req.body.password });
     if (error) {
-        regEr = `Error: ${error.details[0].message}`
+        regEr = "Login and password must be at least 5 characters long!"
+        res.redirect("/register");
+    } else if (user) {
+        regEr = "User with that login already exist. Change login!";
         res.redirect("/register");
     } else {
         let newUser = new User({ login: req.body.login, password: req.body.password });
@@ -68,34 +66,27 @@ app.post('/register', (req, res) => {
 
 
 //SPRAWDZENIE CZY JEST KONTO I OTRZYMANIE LISTY
-app.post('/todo', async function(req, res) {
-    let login = req.body.login;
-    let password = req.body.password;
-    var user = await User.findOne({ login: login, password: password });
+app.post('/todo', async(req, res) => {
+    er = '';
+    let user = await User.findOne({ login: req.body.login, password: req.body.password });
     if (user === null) {
         logEr = 'Incorrect login or password';
         res.redirect('/');
-    }
-    if (user !== null) {
-        er = '';
-        User.find({ login: login, password: password }).then((result) => {
-            console.log(result[0].tasks)
-            res.render('index.mustache', { login: login, tasks: result[0].tasks, er: er });
-        });
+    } else {
+        res.render('index.mustache', { login: req.body.login, tasks: user.tasks, er: er });
     }
 });
 
 //NOWY TASK
 app.post('/newtask', async(req, res) => {
     const { error } = validateTask(req.body.name);
+    let user = await User.findOne({ login: req.body.login })
     if (error) {
-        er = `Error: ${error.details[0].message}`
+        er = "Task cannot be empty"
         logEr = '';
-        User.find({ login: req.body.login }).then((result) => {
-            console.log(result)
-            res.render('index.mustache', { login: result[0].login, tasks: result[0].tasks, er: er })
-        })
+        res.render('index.mustache', { login: user.login, tasks: user.tasks, er: er });
     } else {
+        er = '';
         User.findOneAndUpdate({ login: req.body.login }, { $push: { tasks: req.body.name } }, { new: true, useFindAndModify: false }).then((result) => {
             console.log(result);
             res.render('index.mustache', { login: result.login, tasks: result.tasks, er: er });
@@ -116,7 +107,7 @@ app.post('/deletetask', (req, res) => {
 
 //WALIDACJA TASKA I UŻYTKOWANIKA
 function validateTask(task) {
-    const schema = Joi.string().min(3).required();
+    const schema = Joi.string().required();
 
     return Joi.validate(task, schema);
 }
