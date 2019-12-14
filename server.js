f090ef1beaacf11877e334c0d097c9ff826ee632
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-12-14 10:27:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Task = require('./models/task');
 const User = require('./models/user');
 
 
